ddf827e7cf93df86b8b279dd8bcb6fc59caca3c5
graysonliu <graysonliu@users.noreply.github.com>
2021-04-29 04:00:52 +0000

    deploy: 982c28bb32df32f36f998ef90c3fb3bc3aa9e224

--- make_title.py
+++
@@ -1,7 +0,0 @@
-def make_title(s):
-    s = [c if c.isalnum() else ' ' for c in s.lower()]
-    from datetime import date
-    return str(date.today()) + '-' + '-'.join(''.join(s).split())
-
-
-print(make_title('Python Formatting in VS Code'))
